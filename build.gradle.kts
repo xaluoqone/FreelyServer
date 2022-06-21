8e71dc351af77e3624cf689e0c4f686e02555393
xaluoqone <1617321643@qq.com>
2022-06-21 23:00:09 +0800

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.xaluoqone"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
---
+++ src/main/kotlin/com/xaluoqone/freely/FreelyServerApplication.kt
@@ -0,0 +1,11 @@
+package com.xaluoqone.freely
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreelyServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FreelyServerApplication>(*args)
+}
